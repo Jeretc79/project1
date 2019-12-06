5e624743a29fea84fa1e2865dbef0f93b74e77f3
Jeretc79 <masonjeretc@gmail.com>
2019-12-05 19:11:08 -0800

    updated

---
+++ Project1.js
@@ -0,0 +1,21 @@
+var div = null;
+var markers = [];
+var map = null;
+var geocoder = null;
+let searchTopic = '';
+
+let api_key = "R7HLXwHCCzxfPjBA";
+  //Main API Endpoint for Soundkick
+  
+  let api_endPoint = "http://www.songkick.com/developer";
+  //Resource Endpoints
+  
+  let events = '/events?q='; 
+  let performers = 'performers.slug='; 
+  var results;
+  let venues = '/venues/'; 
+  let artists = [''];
+
+  function createQURL() {
+    querystart = 
+  }
