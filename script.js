2af9ef3e5a612c24c4f428f923c0015083374d58
Sarah Vivoli <snvivoli@gmail.com>
2019-12-07 14:47:41 -0800

    ticketmaster ajax to script file

--- script.js
+++ script.js
@@ -3,7 +3,37 @@ var authKey = 'uyyV4zESWwQbeQrI';
 var artist = 'queen';
 var newURL = baseURL + authKey + '&query=' + artist;
 var baseURL2 = 'https://api.songkick.com/api/3.0/search/locations.json?query=';
-var newURL2 = baseURL2 + city + '&apikey=' + authKey;
+// var newURL2 = baseURL2 + city + '&apikey=' + authKey;
 
 
-console.log(newURL);
+var authKey3 = 'aTm6J9b1v3GF9ZxpISl4sVxEzKNG6hHf';
+var baseURL3 = 'https://app.ticketmaster.com/discovery/v2/events?apikey=' 
+var city3 = 'portland'
+var newURL3 = baseURL3 + authKey3 + '&city=' + city3
+
+console.log(newURL3);
+
+$("#ticket-submit").on('click', function() {
+    var artist = $("#artist-term").val().trim();
+    var results = $("#results");
+    
+    
+
+    $.ajax({
+        url: newURL3,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        for (var i=0; i<response._embedded.events.length; i++) {
+            if (artist === response._embedded.events[i].name) {
+                var artistResult = response._embedded.events[i].name;
+                console.log(artistResult);
+                console.log(response._embedded.events[i].url);
+            }
+        }
+    
+    })
+})
+
+
