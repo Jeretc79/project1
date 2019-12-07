73b273deb37cf19b1d9414fb2feaaa57889838a8
Jeretc79 <masonjeretc@gmail.com>
2019-12-06 17:41:46 -0800

    updated pull

--- Project1.js
+++ Project1.js
@@ -6,16 +6,21 @@ let searchTopic = '';
 
 let api_key = "R7HLXwHCCzxfPjBA";
   //Main API Endpoint for Soundkick
-  
   let api_endPoint = "http://www.songkick.com/developer";
-  //Resource Endpoints
-  
-  let events = '/events?q='; 
+  let events = '/events?q='; //endpoint returns artist and their event.
   let performers = 'performers.slug='; 
   var results;
   let venues = '/venues/'; 
   let artists = [''];
 
-  function createQURL() {
-    querystart = 
+  window.load = getUserLocation();
+
+  function startUpSearch() {
+      
+    var queryURL = "https://api.songkick.com/api/3.0/artists/{artist_id}/calendar.json?apikey=R7HLXwHCCzxfPjBA"
   }
+  
+  
+  function createQURL() {
+    queryStart = "http://www.songkick.com/developer";
+    console.log(queryStart)
