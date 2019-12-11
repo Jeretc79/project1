00411bef0c9a8f0601911aa439b847d8c120679d
Sarah Vivoli <snvivoli@gmail.com>
2019-12-10 19:23:17 -0800

    script file added jQuery divs for title and date, and ticketmaster url

--- script.js
+++ script.js
@@ -2,21 +2,18 @@ var baseURL = 'https://api.songkick.com/api/3.0/search/artists.json?apikey=';
 var baseURL2 = 'https://api.songkick.com/api/3.0/search/locations.json?query=';
 var baseURL3 = 'https://app.ticketmaster.com/discovery/v2/events?apikey=';
 var authKey = 'uyyV4zESWwQbeQrI';
-var authKey3 = 'aTm6J9b1v3GF9ZxpISl4sVxEzKNG6hHf';
+var TMauthKey = 'aTm6J9b1v3GF9ZxpISl4sVxEzKNG6hHf';
 var city = "portland";
 var newURL2 = baseURL2 + city + '&apikey=' + authKey;
-var newURL3 = baseURL3 + authKey3 + '&city=' + city;
+var newURL3 = baseURL3 + TMauthKey + '&size=200&city=' + city;
 var concertsURL;
-
 $("#submitBtn").on('click', function () {
     var artist = $('#artist-term').val().trim();
     var newURL = baseURL + authKey + '&query=' + artist;
-
     $.ajax({
         url: newURL,
         method: "GET"
     }).then(function (results) {
-
         for (var i = 0; i < results.resultsPage.results.artist.length; i++) {
             if (artist === results.resultsPage.results.artist[i].displayName) {
                 var skArtist = results.resultsPage.results.artist[i].displayName;
@@ -28,47 +25,61 @@ $("#submitBtn").on('click', function () {
             }
         }
     });
-
     function giveConcerts() {
-
         $.ajax({
             url: concertsURL,
             method: "GET"
         }).then(function (response) {
-
-            console.log("Secondary Results:" + response);
+            console.log("Secondary response below:")
+            console.log(response);
             var citySearched = $("#city-term").val().trim();
             var citiesIndex = response.resultsPage.results.event.length;
             for (var i = 0; i < citiesIndex; i++) {
                 var songkickCity = response.resultsPage.results.event[i].location.city
                 if (citySearched == songkickCity) {
                     var cityMatch = songkickCity;
-                    console.log("City Match test" + cityMatch);
-                    console.log("SK City:" + response.resultsPage.results.event[i].location.city);
-                    console.log("SK Venue:" + response.resultsPage.results.event[i].venue.displayName);
+                    console.log("City Match test: " + cityMatch);
+                    console.log("SK City: " + response.resultsPage.results.event[i].location.city);
+                    console.log("SK Venue: " + response.resultsPage.results.event[i].venue.displayName);
+                    console.log("Date: " + response.resultsPage.results.event[i].start.date);
+                    console.log("Time: " + response.resultsPage.results.event[i].start.time);
+                    for (var k = 0; k < results.length; k++) {
+                        var concertInfo = $("<div>");
+                        var title = $("<h3>");
+                        title.html(results.resultsPage.results.artist[i].displayName)
+                        var date = $("<h4>");
+                        // date.html()
+
+                        results[k].append(concertInfo);
+                    }
                 }
             }
         })
     };
-
     function getTickets() {
         var artist = $("#artist-term").val().trim();
         var citySearched = $("#city-term").val().trim();
-        var ticketURL = baseURL3 + authKey3 + '&city=' + citySearched;
-
+        var ticketURL = baseURL3 + TMauthKey + '&size=200&city=' + citySearched;
         $.ajax({
             url: ticketURL,
             method: "GET"
         }).then(function (data) {
-            
+            console.log("TM results below:");
             console.log(data);
-            //console.log(response);
             for (var i = 0; i < data._embedded.events.length; i++) {
                 if (artist === data._embedded.events[i].name) {
                     var artistResult = data._embedded.events[i].name;
-                    console.log(artistResult);
-                    console.log(data._embedded.events[i].url);
-                    console.log("tickets ran");
+                    console.log("TM artist: " + artistResult);
+                    console.log("TM url: " + data._embedded.events[i].url);
+                    for (var j = 0; j < results.length; j++) {
+                        var link = $("<a>");
+                        link.attr("href", ticketURL);
+                        link.attr("target", "_blank");
+                        link.text("We Found Tickets Here");
+                        link.addClass("link");
+                        link.attr("style", "font-family: 'Comfortaa', cursive");
+                        results[j].append(link);
+                    }
                 }
             }
         })
