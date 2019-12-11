ee87a09493272288ad43142032dd1627594f9e7b
Howard Kennedy <hkennedyvi@gmail.com>
2019-12-10 19:26:31 -0800

    test HK

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ var baseURL = 'https://api.songkick.com/api/3.0/search/artists.json?apikey=';
 var baseURL2 = 'https://api.songkick.com/api/3.0/search/locations.json?query=';
 var baseURL3 = 'https://app.ticketmaster.com/discovery/v2/events?apikey=';
 var authKey = 'uyyV4zESWwQbeQrI';
-var authKey3 = 'aTm6J9b1v3GF9ZxpISl4sVxEzKNG6hHf';
+var TMauthKey = 'aTm6J9b1v3GF9ZxpISl4sVxEzKNG6hHf';
 var city = "portland";
 var newURL2 = baseURL2 + city + '&apikey=' + authKey;
-var newURL3 = baseURL3 + authKey3 + '&city=' + city;
+var newURL3 = baseURL3 + TMauthKey + '&city=' + city;
 var concertsURL;
+var showTime;
+var timeConvert = moment(showTime, 'HH:mm:ss').format('hh:mm a');
+
+console.log(moment("13:00", 'HH:mm:ss').format('hh:mm a'));
+
 
 
 $("#submitBtn").on('click', function () {
@@ -21,8 +26,8 @@ $("#submitBtn").on('click', function () {
 
         for (var i = 0; i < results.resultsPage.results.artist.length; i++) {
             if (artist === results.resultsPage.results.artist[i].displayName) {
-                var skArtist = results.resultsPage.results.artist[i].displayName;
-                console.log("First function artist:" + skArtist);
+                var songkickArtist = results.resultsPage.results.artist[i].displayName;
+                console.log("First function artist: " + songkickArtist);
 
                 concertsURL = results.resultsPage.results.artist[i].identifier[0].eventsHref + "?apikey=uyyV4zESWwQbeQrI";
                 console.log("Songkick foundation URL: " + concertsURL);
@@ -42,8 +47,8 @@ $("#submitBtn").on('click', function () {
             method: "GET"
         }).then(function (response) {
 
-
-            console.log("Secondary Results:" + response);
+            console.log("Secondary response below:")
+            console.log(response);
 
             var citySearched = $("#city-term").val().trim();
             var citiesIndex = response.resultsPage.results.event.length;
@@ -53,35 +58,45 @@ $("#submitBtn").on('click', function () {
                 var songkickCity = response.resultsPage.results.event[i].location.city
                 if (citySearched == songkickCity) {
                     var cityMatch = songkickCity;
-                    console.log("City Match test" + cityMatch);
-                    console.log("SK City:" + response.resultsPage.results.event[i].location.city);
-                    console.log("SK Venue:" + response.resultsPage.results.event[i].venue.displayName);
+                    var showTime = response.resultsPage.results.event[i].start.time;
+                    console.log("City Match test: " + cityMatch);
+                    console.log("SK City: " + response.resultsPage.results.event[i].location.city);
+                    console.log("SK Venue: " + response.resultsPage.results.event[i].venue.displayName);
+                    console.log("Date: " + response.resultsPage.results.event[i].start.date);
+                    console.log("Time: " + response.resultsPage.results.event[i].start.time);
+                    
+                    console.log("Showtime Test : " + showTime);
+                    console.log("Time Conversion : " + timeConvert);
                 }
             }
 
         })
     };
 
+    
+
     function getTickets() {
         var artist = $("#artist-term").val().trim();
         var citySearched = $("#city-term").val().trim();
-        var ticketURL = baseURL3 + authKey3 + '&city=' + citySearched;
+        var ticketURL = baseURL3 + TMauthKey + '&size=200&city=' + citySearched;
 
         $.ajax({
             url: ticketURL,
             method: "GET"
         }).then(function (data) {
+
+            console.log("TM results below:");
             console.log(data);
-            //console.log(response);
+
             for (var i = 0; i < data._embedded.events.length; i++) {
                 if (artist === data._embedded.events[i].name) {
                     var artistResult = data._embedded.events[i].name;
-                    console.log(artistResult);
-                    console.log(data._embedded.events[i].url);
-                    console.log("tickets ran");
+                    console.log("TM artist: " + artistResult);
+                    console.log("TM url: " + data._embedded.events[i].url);
 
                 }
             }
         })
     }
 });
+
